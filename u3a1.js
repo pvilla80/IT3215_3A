1687cd7c5409242a0944f9365430382fccba7a2e
pvilla80 <41028406+pvilla80@users.noreply.github.com>
2020-10-30 13:11:12 -0500

    Add files via upload

---
+++ u3a1.js
@@ -0,0 +1,28 @@
+var arrImages = ["images/banner1.jpg", "images/banner2.jpg", "images/banner3.jpg"];
+var imageCount = 0;
+function load_images() {
+    var my_image = new Image();
+    i = 0;
+    while (i < arrImages.length) {
+        my_image.src = arrImages[i];
+        i = i + 1;
+    }
+    switchBanner();
+}
+
+function switchBanner() {
+    var elementId = ["Banner1", "Banner2", "Banner3"];
+    if (imageCount === arrImages.length) {
+        imageCount = 0;
+    }
+    i = 0;
+    while (i < elementId.length) {
+        document.getElementById(elementId[i]).src = arrImages[imageCount];
+    }
+    imageCount = imageCount + 1; 
+    setTimeout(switchBanner, 3 * 1000);
+}
+
+function rollImages(firstImage, secondImage) {
+    firstImage.src = secondImage;
+}
