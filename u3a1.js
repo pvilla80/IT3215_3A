054c9eaa83a73e4a586c7b6b766dbcf66c887fda
pvilla80 <41028406+pvilla80@users.noreply.github.com>
2020-10-30 14:26:00 -0500

    Add files via upload

--- u3a1.js
+++ u3a1.js
@@ -4,20 +4,21 @@ function load_images() {
     var my_image = new Image();
     i = 0;
     while (i < arrImages.length) {
-        my_image.src = arrImages[i];
+        my_image.src = arrImages[i] + 1;
         i = i + 1;
     }
     switchBanner();
 }
 
 function switchBanner() {
-    var elementId = ["Banner1", "Banner2", "Banner3"];
+    var elementId = ["Banner1" , "Banner2" , "Banner3"];
     if (imageCount === arrImages.length) {
         imageCount = 0;
     }
     i = 0;
     while (i < elementId.length) {
-        document.getElementById(elementId[i]).src = arrImages[imageCount];
+        document.getElementById(elementId[i]).src = arrImages[imageCount] + 1;
+        i = i + 1;
     }
     imageCount = imageCount + 1; 
     setTimeout(switchBanner, 3 * 1000);
